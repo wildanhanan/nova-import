63facce689cb1f0eb4e938c10ad1f77ae59121b2
Wildan Hanan Assyauqi <wildanhanan182@gmail.com>
2026-07-17 10:52:43 +0700

    Remove example database configuration

--- api/config.example.php
+++ api/config.example.php
@@ -1,7 +1,1 @@
-<?php
 
-define('DB_HOST', 'YOUR_DB_HOST');
-define('DB_PORT', 'YOUR_DB_PORT');
-define('DB_NAME', 'YOUR_DB_NAME');
-define('DB_USER', 'YOUR_DB_USER');
-define('DB_PASS', 'YOUR_DB_PASSWORD');
