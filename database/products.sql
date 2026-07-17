f5bfe6c5e223784a50112924692ac2bf19eefe00
Wildan Hanan Assyauqi <wildanhanan182@gmail.com>
2026-07-17 11:01:44 +0700

    Clean up products.sql by removing comments and settings
    Removed unnecessary comments and SQL settings from the products.sql file.

--- database/products.sql
+++ database/products.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: sql202.infinityfree.com
--- Waktu pembuatan: 16 Jul 2026 pada 23.47
--- Versi server: 11.4.12-MariaDB
--- Versi PHP: 7.2.22
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fake_store_db`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
